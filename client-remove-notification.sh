d2ca9618ee0f2a5a15741543ddec5b2f0700c4cd
hillz2 <galih.prastowo@gmail.com>
2024-02-08 12:43:29 +0700

    Update client-remove-notification.sh

--- client-remove-notification.sh
+++ client-remove-notification.sh
@@ -12,18 +12,19 @@ send_to_telegram() {
 if [[ ! -f /tmp/wifi_clients.txt ]]; then
         cat /tmp/dhcp.leases | awk '{print $3,$4,$5}' > /tmp/wifi_clients.txt
 fi
+device_name=$(cat /tmp/sysinfo/model | sed 's/[()_-]//;s/ //g')
 
 while read -r line; do
     ip_address=$(echo "${line}" | awk '{print $1}')
-    hostname=$(echo "${line}" | awk '{print $2}')
+    hostname=$(echo "${line}" | awk '{print $2}' | sed 's/ //')
     mac_address=$(echo "${line}" | awk '{print $3}')
 
-    ping -c 3 -W 3 "${ip_address}" > /dev/null 2>&1
+    ping -c 1 -W 5 "${ip_address}" > /dev/null 2>&1
     if [[ $? != 0 ]]; then # If client is DISCONNECTED from Wi-Fi
         sed -i "/${ip_address} ${hostname} ${mac_address}/d" /tmp/wifi_clients.txt
         local_msg="New device REMOVED from the network, sending it to telegram..."
         logger -p local0.info -t dhcp-remove-notify "$local_msg"
-        send_to_telegram "New device REMOVED from $(cat /tmp/sysinfo/model | sed 's/[()_-]//g'):
+        send_to_telegram "\#${hostname} REMOVED from \#${device_name}:
 \`\`\`
 Time: $(date "+%A %d-%b-%Y %T")
 Hostname: ${hostname}
